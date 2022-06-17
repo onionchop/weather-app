79d780acd7bfabd775e47f2bb2b74c0a994a164d
onionchop <ruthhyo@gmail.com>
2022-06-17 00:13:49 -0400

    started on README

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 // w/ key
 
 
-fetch ('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=be2f9b736692d924bd1c98d748a23042' )
+fetch ('https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${weatherKey}' )
     .then(response => response.json())
     .then(data => {
         weatherRes(data)
@@ -12,16 +12,20 @@ fetch ('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exc
 
 
 function weatherRes(resultObj){
-    var latitude = resultObj[0].latitude;
-    var longitude = resultObj[0].longitude;
+    var latitude = resultObj[0].lat;
+    var longitude = resultObj[0].lon;
 
-    var weatherCall = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely&appid=be2f9b736692d924bd1c98d748a23042'
+    var weatherFetch = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely&appid=be2f9b736692d924bd1c98d748a23042'
 
-    fetch(weatherCall)
+    fetch(weatherFetch)
     .then(response => response.json())
     .then(data => {
-
+        weatherCall(data)
     });
 }
 
-// function 
+function weatherCall(resultObj){
+    $("#city").text(userInput);
+
+    $("#date").text()
+}
